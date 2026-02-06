a79de3591d12a4da86f0e20c59d2099f7bb5d875
Mashhura <abdurasulovamashhura15@gmail.com>
2026-02-06 15:27:44 +0500

    Create oylar.py

---
+++ oylar.py
@@ -0,0 +1,5 @@
+# Yil oylari ro'yxati 
+oylar=["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"]
+print("Yil oylari:")
+for oy in oylar 
+print("-"+oy)
